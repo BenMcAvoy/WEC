344a8c1ede57c0d25ce4f3e301f5171b59e91219
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-02-27 22:11:59 +0000

    Fix accuracy issues when setting position

--- src/client/java/benmcavoy/wecclient/WecClientClient.java
+++ src/client/java/benmcavoy/wecclient/WecClientClient.java
@@ -14,6 +14,7 @@ import net.minecraft.command.argument.BlockStateArgumentType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,26 +25,25 @@ public class WecClientClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			LiteralCommandNode<FabricClientCommandSource> wecCommand = ClientCommandManager
-					.literal("wec")
+					.literal("set")
 					.build();
 
 			ArgumentCommandNode<FabricClientCommandSource, BlockStateArgument> setCommand = ClientCommandManager
 					.argument("block", BlockStateArgumentType.blockState(registryAccess))
 					.executes((ctx) -> {
-						Pair<Vec3d, Vec3d> pos = ((MouseMixinAccess) MinecraftClient.getInstance().mouse).wecclient$access();
-						Vec3d pos1 = pos.getLeft();
-						Vec3d pos2 = pos.getRight();
+						Pair<Vec3i, Vec3i> pos = ((MouseMixinAccess) MinecraftClient.getInstance().mouse).wecclient$access();
+						Vec3i pos1 = pos.getLeft();
+						Vec3i pos2 = pos.getRight();
 
 						BlockState block = ctx.getArgument("block", BlockStateArgument.class).getBlockState();
 						String blockName = block.toString();
-						MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of(blockName));
 
 						String[] split = blockName.split("}", 2);
 						String blockNameStripped = split[0].substring(6);
 						String blockStateData = split.length > 1 ? split[1] : "";
 
 						assert MinecraftClient.getInstance().player != null;
-						String command = String.format("fill %d %d %d %d %d %d %s%s", (int) pos1.x, (int) pos1.y, (int) pos1.z, (int) pos2.x, (int) pos2.y, (int) pos2.z, blockNameStripped, blockStateData);
+						String command = String.format("fill %d %d %d %d %d %d %s%s", pos1.getX(), pos1.getY(), pos1.getZ(), pos2.getX(), pos2.getY(), pos2.getZ(), blockNameStripped, blockStateData);
 
                         MinecraftClient.getInstance().player.networkHandler.sendChatCommand(command);
 
--- src/client/java/benmcavoy/wecclient/mixin/client/MouseMixin.java
+++ src/client/java/benmcavoy/wecclient/mixin/client/MouseMixin.java
@@ -4,13 +4,14 @@ import benmcavoy.wecclient.MouseMixinAccess;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
-import net.minecraft.client.Keyboard;
 
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Pair;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,70 +20,51 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
 public class MouseMixin implements MouseMixinAccess {
-	@Unique
-	private final MinecraftClient client = MinecraftClient.getInstance();
+    @Unique
+    private final MinecraftClient client = MinecraftClient.getInstance();
 
-	@Unique
-	public Vec3d pos1 = null;
-	@Unique
-	public Vec3d pos2 = null;
+    @Unique
+    public Vec3i pos1 = null;
+    @Unique
+    public Vec3i pos2 = null;
 
-	@Inject(at = @At("HEAD"), method = "onMouseButton")
-	private void onMouseButton(long window, int button, int action, int mods, CallbackInfo info) {
-		// If we pressed a mouse button
-		if (action != 1)
-			return;
+    @Inject(at = @At("HEAD"), method = "onMouseButton")
+    private void onMouseButton(long window, int button, int action, int mods, CallbackInfo info) {
+        ClientPlayerEntity player = client.player;
 
-		// If we pressed the middle mouse button (2) then reset the positions
-		if (button == 2) {
-			client.inGameHud.getChatHud().addMessage(Text.of("Reset"));
-			pos1 = null;
-			pos2 = null;
-		}
+        // If we pressed a mouse button
+        if (action != 1
+                || player == null
+                || client.world == null
+                || client.crosshairTarget == null
+                || client.crosshairTarget.getType() != HitResult.Type.BLOCK
+                || player.getMainHandStack().getItem() != Items.WOODEN_SWORD
+                || client.crosshairTarget.getPos().distanceTo(player.getPos()) > 5) {
+            return;
+        }
 
-		// If we have a client
-		if (client.player == null)
-			return;
+        // If we pressed the middle mouse button (2) then reset the positions
+        if (button == 2) {
+            client.inGameHud.getChatHud().addMessage(Text.of("WEC » Reset"));
+            pos1 = null;
+            pos2 = null;
+        }
 
-		// If we are holding a wood sword
-		if (client.player.getMainHandStack().getItem() != net.minecraft.item.Items.WOODEN_SWORD)
-			return;
+        // Get the block we are looking at
+        Vec3i hitVec = ((BlockHitResult) client.crosshairTarget).getBlockPos();
 
-		// If we have a crosshair target
-		if (client.crosshairTarget == null)
-			return;
+        // Set position 1 or 2
+        if (button == 0) {
+            client.inGameHud.getChatHud().addMessage(Text.of("WEC » Position 1 set"));
+            pos1 = hitVec;
+        } else if (button == 1) {
+            client.inGameHud.getChatHud().addMessage(Text.of("WEC » Position 2 set"));
+            pos2 = hitVec;
+        }
+    }
 
-		// Check if it's a block and in range
-		if (client.crosshairTarget.getType() != net.minecraft.util.hit.HitResult.Type.BLOCK)
-			return;
-
-		// If we have a world
-		if (client.world == null)
-			return;
-
-		// Get the block we are looking at
-		BlockHitResult hit = (BlockHitResult) client.crosshairTarget;
-
-		// Get the coordinates of the block and not the coordinates of the mouse on the block
-		Vec3d hitVec = hit.getBlockPos().toCenterPos();
-
-		Vec3d d = new Vec3d(hitVec.x, hitVec.y, hitVec.z - 1);
-
-		if (hit.getType() != HitResult.Type.BLOCK)
-			return;
-
-		// Set position 1 or 2
-		if (button == 0) {
-			client.inGameHud.getChatHud().addMessage(Text.of("Position 1 set"));
-			pos1 = d;
-		} else if (button == 1) {
-			client.inGameHud.getChatHud().addMessage(Text.of("Position 2 set"));
-			pos2 = d;
-		}
-	}
-
-	@Override
-	public Pair<Vec3d, Vec3d> wecclient$access() {
-		return new Pair<>(pos1, pos2);
-	}
+    @Override
+    public Pair<Vec3i, Vec3i> wecclient$access() {
+        return new Pair<>(pos1, pos2);
+    }
 }
